d9a4e98dd5b2414facf452a6bbb1f4baeed73d7c
Vladislav <tm_vladislav@mail.ru>
2020-01-23 17:52:36 +0200

    add Employee

---
+++ src/main/java/Employee.java
@@ -0,0 +1,57 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Employee {
+
+    private String name,surname,address;
+    private Calendar date;
+    private Gender sex;
+    private double salary;
+
+
+    public Employee(String name, String surname,Gender sex, byte day, byte month, int year, String address, double salary) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.sex = sex;
+        this.salary = salary;
+        date = new GregorianCalendar(year,--month,day);
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getSurname(){
+        return surname;
+    }
+    public Gender getSex() {
+        return sex;
+    }
+
+    public String toString() {
+        return "Name: " + name + "\r\nSurname: " + surname + "\r\nSex: " + sex.toString() + "\r\nAddress: " + address + "\r\nSalary: " + salary +
+               "\r\nДень: " +  date.get(Calendar.DAY_OF_MONTH) + " Месяц: " + (date.get(Calendar.MONTH) +1) + " Год: " + date.get(Calendar.YEAR);
+    }
+
+
+    @Override
+    public boolean equals(Object obj){
+        if (!(obj instanceof Employee)) return false;
+
+        Employee temp = (Employee) obj;
+
+        return this.name.equals(temp.getName())
+            && this.surname.equals(temp.getSurname())
+            && this.sex == temp.getSex();
+    }
+    @Override
+    public int hashCode() {
+        int result = 22;
+        result = 228 * result + name.hashCode();
+        result = 228 * result + surname.hashCode();
+        result = 228 * result + sex.hashCode();
+        return result;
+    }
+
+   
+}
---
+++ src/main/java/EmployeeManager.java
@@ -0,0 +1,91 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class EmployeeManager {
+
+    private static EmployeeManager instance;
+    private Set<Employee> list;
+
+    private EmployeeManager() {
+        list = new HashSet<>();
+    }
+
+    public static EmployeeManager getInstance() {
+        if (instance == null) {
+            instance = new EmployeeManager();
+        }
+        return instance;
+    }
+
+    public int GetEmployeeCount() {
+        return list.size();
+    }
+
+    public void addEmployee(Object[] obj) {
+
+        try {
+            String[] mas = Arrays.copyOf(obj, obj.length, String[].class);
+
+            try {
+                Gender s = mas[2].equals("Male") ? Gender.MALE : Gender.FEMALE;
+                
+                String[] date = mas[3].split("\\.");
+
+                byte d = Byte.parseByte(date[0]);
+                byte m = Byte.parseByte(date[1]);
+                int y = Integer.parseInt(date[2]);
+                double salary = Double.parseDouble(mas[4]);
+
+                list.add(new Employee(mas[0], mas[1], s, d, m, y, mas[5], salary));
+
+                
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public void showEmployee() {
+        Iterator iter = list.iterator();
+        for (; iter.hasNext();) {
+
+            System.out.println(iter.next().toString());
+        }
+    }
+
+    public void exportEmployee(String patch) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(patch))) {
+
+            for (Employee emp : list) {
+                bufferedWriter.write(emp.toString());
+                bufferedWriter.write("\r\n************************************************\r\n");
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public Employee searchEmployee(String nameAndSurname) {
+        for (Employee emp : list) {
+            String temp = (emp.getName() + " " + emp.getSurname()).toUpperCase();
+            String temp2 = (emp.getSurname() + " " + emp.getName()).toUpperCase();
+            if (nameAndSurname.equalsIgnoreCase(temp) || nameAndSurname.equalsIgnoreCase(temp2)) {
+                return emp;
+            }
+        }
+        return null;
+    }
+
+    public boolean deleteEmployee(String nameAndSurname) {
+        Employee emp = this.searchEmployee(nameAndSurname);
+        return list.remove(emp);
+    }
+
+}
